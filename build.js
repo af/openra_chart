66feb982cfa30a50f3cf4e00eab54426b229d8b7
Aaron Franks <aaron.franks@gmail.com>
2014-03-16 20:48:48 -0700

    Write units to json and only deal with specific input files.

--- build.js
+++ build.js
@@ -1,13 +1,18 @@
+// Convert OpenRA's YAML-ish unit files to json
+
 var fs = require('fs');
 var path = require('path');
 var yaml = require('yamljs');
 
 var YAML_DIR = '../openra/mods/ra/rules';
+var filesToCheck = ['vehicles', 'aircraft', 'infantry', 'ships'];
 
 var filenames = fs.readdirSync(YAML_DIR);
 var contents = {};
-filenames.map(function(f) {
+filenames.forEach(function(f) {
     var key = f.split('.')[0];
+    if (filesToCheck.indexOf(key) === -1) return;
+
     var txt = fs.readFileSync(path.join(YAML_DIR, f), 'utf8');
     var detabbed = txt.replace(/\t/g, '    ');     // convert tabs to spaces
     console.log('parsing', key);
@@ -18,5 +23,5 @@ filenames.map(function(f) {
     }
 });
 
-//console.log(contents);
-console.log(contents.aircraft.MIG);
+fs.writeFileSync('units.json', JSON.stringify(contents, null, 2));
+console.log('---\nWrote units.json');
