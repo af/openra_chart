b86eecb7f2300cba7404e454833cb03a25fa5418
Aaron Franks <aaron.franks@gmail.com>
2014-03-19 21:30:12 -0700

    WIP for switching y-axis values on the fly

--- js/main.js
+++ js/main.js
@@ -1,39 +1,45 @@
 var d3 = require('d3');
 var svg = d3.select('svg');
-var xScale = d3.scale.linear();
-var yScale = d3.scale.linear();
-var xAxis = d3.svg.axis();
-var yAxis = d3.svg.axis();
 
 var margin = { top: 30, left: 30, right: 30, bottom: 30 };
 var width = window.innerWidth;
 var height = 600;
 
-function attachData(data) {
-    xScale.domain([0, d3.max(data, function(d) { return d.Valued.Cost; })])
+var xScale = d3.scale.linear();
+var yScale = d3.scale.linear();
+
+// Setup axes:
+var xAxis = d3.svg.axis();
+var yAxis = d3.svg.axis();
+var xAxisEl = svg.append('g').attr('class', 'axis')
+                .attr('transform', 'translate(0,' + (height - margin.top - margin.bottom) + ')');
+var yAxisEl = svg.append('g').attr('class', 'axis')
+                .attr('transform', 'translate(' + margin.left + ',0)');
+
+
+function renderChart(data, yFn) {
+    var xValueFn = function(d) { return xScale(d.Valued.Cost) };
+    var yValueFn = function(d, i) { return yScale(yFn(d,i)); };
+
+    // Set up x/y scales and axes for the given data:
+    xScale.domain([0, d3.max(data, xValueFn)])
           .range([margin.left, width - margin.left - margin.right]);
     xAxis.scale(xScale).orient('bottom');
-    svg.append('g').attr('class', 'axis')
-        .call(xAxis)
-        .attr('transform', 'translate(0,' + (height - margin.top - margin.bottom) + ')');
+    xAxisEl.call(xAxis);
 
-    yScale.domain([0, d3.max(data, function(d) { return d.Health.HP; })])
+    yScale.domain([0, d3.max(data, yFn)])
           .range([height - margin.top - margin.bottom, margin.top]);
     yAxis.scale(yScale).orient('left');
-    svg.append('g').attr('class', 'axis')
-        .call(yAxis)
-        .attr('transform', 'translate(' + margin.left + ',0)');
+    yAxisEl.call(yAxis);
 
     var enter = svg.selectAll('g.unit')
                     .data(data)
                     .enter();
     var groups = enter.append('g').attr('class', 'unit');
 
-    var x = function(d) { return xScale(d.Valued.Cost) };
-    var y = function(d) { return yScale(d.Health.HP) };
     groups.append('circle')
-        .attr('cx', x)
-        .attr('cy', y)
+        .attr('cx', xValueFn)
+        .attr('cy', yValueFn)
         .attr('r', 5)
         .attr('class', function(d) {
             var owner = (d.Buildable || {}).Owner;
@@ -41,9 +47,15 @@ function attachData(data) {
         });
 
     groups.append('text')
-        .attr('x', x)
-        .attr('y', y)
+        .attr('x', xValueFn)
+        .attr('y', yValueFn)
         .text(function(d) { return (d.Tooltip || {}).Name });
 }
 
-attachData(window.units);
+window.a = function() {
+    renderChart(window.units, function(d) { return (d.Health.HP) });
+};
+window.b = function() {
+    renderChart(window.units, function(d) { return ((d.RevealsShroud || {}).Range) || null });
+};
+window.r = renderChart;
